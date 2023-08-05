db24da916491986eee3d7ee6cf19859149f1e9a6
e-0a <118397019+e-0a@users.noreply.github.com>
2023-08-04 23:48:17 -0300

    -

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,8 @@ app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
 
   try{
+    const r = await axios.put('https://api.chatengine.io/users/', { username: username, secret: username, first_name: username },
+
   } catch (e) {
 
   }
