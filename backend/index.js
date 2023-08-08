1e104986ddcf57ea3a2eb565b972a3b67ce739e3
e-0a <118397019+e-0a@users.noreply.github.com>
2023-08-08 15:35:35 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,6 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-
   try {
     const r = await axios.put('https://api.chatengine.io/users/',
       { username: username, secret: username, first_name: username },
